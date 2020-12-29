e6608f00837e9e0bb0d59977c924e90e21c9f692
Teoman00 <harunselimgedikli@hotmail.com>
2020-12-29 15:26:31 +0300

    Minor env fix.

--- src/main/java/me/fluxteam/fluxbot/utils/JSONUtilities.java
+++ src/main/java/me/fluxteam/fluxbot/utils/JSONUtilities.java
@@ -1,6 +1,5 @@
 package me.fluxteam.fluxbot.utils;
 
-import me.fluxteam.fluxbot.Private;
 import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONObject;
 
@@ -13,10 +12,10 @@ public class JSONUtilities {
         JSONObject json = new JSONObject();
         json.put("type","service_account");
         json.put("project_id", "fluxbotbyft");
-        json.put("private_key_id", Private.private_key_id);
-        json.put("private_key", Private.private_key);
-        json.put("client_email", Private.client_email);
-        json.put("client_id", Private.client_id);
+        json.put("private_key_id", System.getenv("PRIVATE_KEY_ID"));
+        json.put("private_key", System.getenv("PRIVATE_KEY"));
+        json.put("client_email", System.getenv("CLIENT_EMAIL"));
+        json.put("client_id", System.getenv("CLIENT_ID"));
         json.put("auth_uri", "https://accounts.google.com/o/oauth2/auth");
         json.put("token_uri", "https://oauth2.googleapis.com/token");
         json.put("auth_provider_x509_cert_url", "https://www.googleapis.com/oauth2/v1/certs");
